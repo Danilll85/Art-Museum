5c75cf2202f1c942be5c8b77feded76997c3a80b
Danilll85 <danilakuroedov85@gmail.com>
2025-04-03 18:01:22 +0300

    update logo

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -5,7 +5,6 @@ import bookmark_orange_fill from './bookmark-orange-fill.svg'
 import home from './home.svg'
 import loader_image from './image-loader.svg'
 import loader from './loader.svg'
-import modsen_logo from './modsen-logo.svg'
 import museum_logo from './museum-logo.svg'
 import museum_logo_black from './museum-logo-black.svg'
 import museum_logo_icon from './museum-logo-icon.svg'
@@ -18,7 +17,6 @@ export const images = {
   bookmark,
   home,
   loader,
-  modsen_logo,
   museum_logo_black,
   museum_logo_icon,
   museum_logo,
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <FooterStyled>
       <img src={images.museum_logo_black} alt='Museum logo' />
-      <img src={images.modsen_logo} alt='Museum logo' />
+      <span>Developed by Danila Kuroedov</span>
     </FooterStyled>
   )
 }
